21abda2685b17d33389678a5c058033aec8a6600
kumar-b5 <kumar_b5@rediffmail.com>
2016-10-08 17:27:54 -0400

    Update vaccine.go

--- Chaincode/vaccine.go
+++ Chaincode/vaccine.go
@@ -752,11 +752,10 @@ func (t *SimpleChaincode) record_temp(stub *shim.ChaincodeStub, v Vehicle, calle
 
 	if err != nil { return nil, errors.New("GET_VEHICLE_DETAILS: Invalid vehicle object") }
 
-	getpos = len(v.Temp)
-	v.Temp.Temptime[getpos+1] = args[0]
-	v.Temp.Temperature[getpos+1] = args[1]
+	getpos := len(v.Temp)
+	v.Temp.Temptime[getpos+1] := args[0]
+	v.Temp.Temperature[getpos+1] := args[1]
 
-	_, err := t.save_changes(stub, v)
 
              //Log the Temperature to the Vaccine
 	} else {
